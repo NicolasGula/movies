850dd341c126af2ae6ab9d011dc61461e4a25082
NicolasBonzini <nicolas.bonzini@cencosud.com>
2022-10-23 09:38:17 -0300

    Armado del sistema de rutas con React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "antd";
+
+//pages
+import Home from "./pages/home";
+import Error404 from "./pages/error404";
+import Movie from "./pages/movie";
+import NewMovie from "./pages/new-movies";
+import Popular from "./pages/popular";
+import Search from "./pages/search";
+
 function App() {
+  const { Header, Content } = Layout;
+
   return (
-    <div className="App">
-      <h1>Movie app</h1>
-    </div>
+    <Layout>
+      <Header>Header....</Header>
+
+      <Content>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new-movies" element={<NewMovie />} />
+          <Route path="/movie/:id" element={<Movie />} />
+          <Route path="/popular" element={<Popular />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/*" element={<Error404 />} />
+        </Routes>
+      </Content>
+    </Layout>
   );
 }
 
